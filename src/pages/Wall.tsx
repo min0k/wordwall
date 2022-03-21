39e208a0453d6e2cf52c9c1b531e4e7c4e1cf02f
min0k <mino.kim@minokim.com>
2022-03-21 13:12:01 -0700

    Fetch all words from server

--- src/pages/Wall.tsx
+++ src/pages/Wall.tsx
@@ -1,6 +1,17 @@
 import Form from "../components/Form";
+import { useEffect } from "react";
 
 export default function Wall() {
+  useEffect(() => {
+    console.log("Hello");
+    fetch("https://wordwallserver-et57m7q3ca-uw.a.run.app/all-words").then(
+      (res) => {
+        res.json().then((data) => {
+          console.log(data);
+        });
+      }
+    );
+  });
   return (
     <div>
       <Form />
